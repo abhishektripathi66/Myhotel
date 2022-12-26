a6e36195ffb72f8e8f7f3468e1e3f47e3de52a99
abhishektripathi66 <abhishekji66@gmail.com>
2022-12-26 08:29:58 +0530

    adding recipes, shared, shopping list

---
+++ Myhotel/src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+
+    constructor(public name:string, public amount: number){
+        // this.name=name;
+        // this.amount=amount;
+    }
+}
